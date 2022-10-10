fbb1b7b32a3764ed0ea75ea4cec7005f59dbadee
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-10 23:25:38 +0200

    ecuacion 2

--- Clase/ejercicio.py
+++ Clase/ejercicio.py
@@ -1,11 +1,25 @@
-import sympy
-x = sympy.Symbol("x")
-y = sympy.Function("y")
+import sympy as sp
+x = sp.Symbol("x")
+y = sp.Function("y")
+
+def ecuacion1():
+    
+    a = y(x).diff(x)
+    ec1 = x**2*y(x)-y(x) / y(x)+1
+    a1 =sp.Eq(a, ec1) #expresar ecuacion
+    print("La ecuacion es:", a1)
+    solucion = sp.dsolve(a-ec1) #resolver ecuacion
+    print(solucion)
+    
+ecuacion1()
 
 
-a = y(x).diff(x)
-ec1 = x**2*y(x)-y(x) / y(x)+1
-a1 =sympy.Eq(a, ec1) #expresar ecuacion
-print("La ecuacion es:", a1)
-solucion = sympy.dsolve(a-ec1) #resolver ecuacion
-print(solucion)
+def ecuacion2():
+    b = y(x).diff(x)
+    ec2 = (y(x)*sp.log(y(x)))/sp.sin(x) #con sympy log"=="ln
+    a2 =sp.Eq(b, ec2)
+    print("La ecuacion es:", a2)
+    solucion2 = sp.dsolve(b-ec2)
+    print(solucion2)
+    
+ecuacion2()
