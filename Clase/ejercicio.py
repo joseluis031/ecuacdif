d4185c60153d8abd1e415a7f645d9a912e5c6a10
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-11 10:23:14 +0200

    poo

--- Clase/ejercicio.py
+++ Clase/ejercicio.py
@@ -2,16 +2,29 @@ import sympy as sp
 x = sp.Symbol("x")
 y = sp.Function("y")
 
-def ecuacion1():
+a = y(x).diff(x)
+
+
+class Ecuacion1:
+    def __init__(self,x,y,a):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.ec1 = x**2*y(x)-y(x) / y(x)+1
+        self.a1 =sp.Eq(a, self.ec1) #expresar ecuacion
+
+
+    def __str__(self):
+        return f"La ecuacion 1 es: {self.a1}"
+        
+    def resolver(self):
+        
+        solucion = sp.dsolve(a-self.ec1) #resolver ecuacion
+        print("Su solucion:",solucion)
     
-    a = y(x).diff(x)
-    ec1 = x**2*y(x)-y(x) / y(x)+1
-    a1 =sp.Eq(a, ec1) #expresar ecuacion
-    print("La ecuacion 1 es:", a1)
-    solucion = sp.dsolve(a-ec1) #resolver ecuacion
-    print("Su solucion:",solucion)
-    
-ecuacion1()
+sol1 = Ecuacion1
+print(sol1)
+sol1.resolver()
 
 
 def ecuacion2():
